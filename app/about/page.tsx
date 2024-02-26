9225bd3c535f20c7b26b53ef82448dd402777311
marcelopaivag <mff.paiva@gmail.com>
2024-02-26 18:20:26 -0300

    ahora si

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
 
-export const metadata: Metadata = {
-    title: "Acerca de",
-    description: "Acerca de Kinesiólogo Marcelo Paiva",
-  };
+"use client"
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+const AboutPage = () => {
+  const router = useRouter()
   return (
-    <div>about</div>
+    <section>
+      <h1>Acerca de</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum nisi officia suscipit ullam earum non accusantium corporis asperiores, alias sapiente? Id, accusantium suscipit cupiditate nisi totam doloremque aliquid atque quibusdam?</p>
+
+      <button className='bg-sky-400 px-3 py-2 rounded-md'
+      onClick={()=>{
+        router.push('/')
+      }}>
+        Volver al inicio
+      </button>
+
+    </section>
   )
 }
 
-export default page
+export default AboutPage

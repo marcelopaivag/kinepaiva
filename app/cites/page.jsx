fd9ccc241e2400fd0a1cde2f2b926f8d46b176c7
marcelopaivag <mff.paiva@gmail.com>
2024-03-02 17:49:36 -0300

    pre modificación de prisma scheme

--- app/cites/page.jsx
+++ app/cites/page.jsx
@@ -1,24 +1,28 @@
 "use client"
-import React from 'react'
 
 const page = () => {
+
   const onSubmit = async (e) => {
     e.preventDefault()
-    const nombre = e.target.nombre.value
-    const apellido = e.target.apellido.value
-    const telefono = e.target.telefono.value
-    const correo = e.target.correo.value
-    const fechaHora = e.target.fechaHora.value
+    const name = e.target.name.value;
+    const lastname = e.target.lastname.value;
+    const phone = e.target.phone.value;
+    const mail = e.target.mail.value;
+    const dateTime = e.target.dateTime.value;
 
     const res = await fetch('api/cites', {
       method: 'POST',
-      body: JSON.stringify({ nombre, apellido, telefono, correo, fechaHora }),
+      body: JSON.stringify({ name, lastname, phone, mail, dateTime }),
       headers: {
         'Content-Type': 'application/json'
       }
     })
     const data = await res.json()
     console.log(data)
+
+
+
+
   }
 
 
@@ -27,47 +31,47 @@ const page = () => {
       <h2 className="text-2xl font-bold mb-8">Reserva tu hora</h2>
       <div className="flex flex-wrap -mx-3">
         <div className="w-full md:w-1/2 px-3">
-          <label htmlFor="nombre">Nombre:</label>
+          <label htmlFor="name">Nombre:</label>
           <input
             type="text"
-            id="nombre"
-            name="nombre"
+            id="name"
+            name="name"
             className="border-2 border-gray-300 rounded p-2 mb-6 w-full text-black"
           />
         </div>
         <div className="w-full md:w-1/2 px-3">
-          <label htmlFor="apellido">Apellido:</label>
+          <label htmlFor="lastname">Apellido:</label>
           <input
             type="text"
-            id="apellido"
-            name="apellido"
+            id="lastname"
+            name="lastname"
             className="border-2 border-gray-300 rounded p-2 mb-6 w-full text-black"
           />
         </div>
         <div className="w-full px-3">
-          <label htmlFor="telefono">Teléfono:</label>
+          <label htmlFor="phone">Teléfono:</label>
           <input
-            type="tel"
-            id="telefono"
-            name="telefono"
+            type="number"
+            id="phone"
+            name="phone"
             className="border-2 border-gray-300 rounded p-2 mb-6 w-full text-black"
           />
         </div>
         <div className="w-full px-3">
-          <label htmlFor="correo">Correo electrónico:</label>
+          <label htmlFor="mail">Correo electrónico:</label>
           <input
             type="email"
-            id="correo"
-            name="correo"
+            id="mail"
+            name="mail"
             className="border-2 border-gray-300 rounded p-2 mb-6 w-full text-black"
           />
         </div>
         <div className="w-full px-3">
-          <label htmlFor="fechaHora">Fecha y hora:</label>
+          <label htmlFor="dateTime">Fecha y hora:</label>
           <input
             type="datetime-local"
-            id="fechaHora"
-            name="fechaHora"
+            id="dateTime"
+            name="dateTime"
             className="border-2 border-gray-300 rounded p-2 mb-6 w-full text-black"
           />
         </div>
